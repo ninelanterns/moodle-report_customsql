1b92dfc1e82832fe54d9f0706952d8db537fcb7e
kausweb <kaushtuvgurung@gmail.com>
2020-03-27 08:53:22 +1100

    HESHAS-453 Bugfix Report custom sql

--- locallib.php
+++ locallib.php
@@ -308,10 +308,10 @@ function report_customsql_print_reports_for($reports, $type) {
                               array('href' => report_customsql_url('view.php?id='.$report->id))).
              ' '.report_customsql_time_note($report, 'span');
         if ($canedit) {
-            $imgedit = html_writer::tag('img', '', array('src' => $OUTPUT->pix_url('t/edit'),
+            $imgedit = html_writer::tag('img', '', array('src' => $OUTPUT->pix_icon('t/edit'),
                                                          'class' => 'iconsmall',
                                                          'alt' => get_string('edit')));
-            $imgdelete = html_writer::tag('img', '', array('src' => $OUTPUT->pix_url('t/delete'),
+            $imgdelete = html_writer::tag('img', '', array('src' => $OUTPUT->pix_icon('t/delete'),
                                                            'class' => 'iconsmall',
                                                            'alt' => get_string('delete')));
             echo ' '.html_writer::tag('span', get_string('availableto', 'report_customsql',
